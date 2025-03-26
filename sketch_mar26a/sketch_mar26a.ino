584541a72c485c507778df8b18b95c0829914cc0
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-26 19:14:00 +0800

    LCD working

--- sketch_mar26a/sketch_mar26a.ino
+++ sketch_mar26a/sketch_mar26a.ino
@@ -11,10 +11,49 @@ void setup() {
   lcd.setCursor(0,0);
   lcd.print("G");
   lcd.setCursor(0,1);
-  lcd.print("DST");
+  lcd.print("DST"); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+    String messages[] = {
+    "DST        ",
+    "Tara        ",
+    "Carry me    ",
+    "I'm a Wigfrid"
+  };
+
+  for(int i = 0; i<4; i++)
+  {
+    lcd.setCursor(0,1);
+    lcd.print(messages[i]);
+    delay(1000);
+
+    lcd.clear();
+  }
+
+/* for(int i = 0; i<= 10; i++)
+{
+  lcd.setCursor(0,1);
+  lcd.print(i);
+  delay(1000);
+} */
+/*   lcd.setCursor(0,1);
+  lcd.print("Wilson");
+  delay(1000);
+  lcd.print("Willow");
+  delay(1000);
+  lcd.print("Wolfgang");
+  delay(1000);
+  lcd.print("Wendy");
+  delay(1000);
+  lcd.print("Woodie");
+  delay(1000);
+  lcd.print("Maxwell");
+  delay(1000);
+  lcd.print("Webber");
+  delay(1000);
+  lcd.print("Wigfried");
+  delay(1000); */
 }
