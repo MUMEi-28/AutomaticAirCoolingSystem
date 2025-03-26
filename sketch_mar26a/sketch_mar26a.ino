0c61df634c398bd971dd491be2addaaa5b66ecb0
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-26 23:22:07 +0800

    LCD with sensors

--- sketch_mar26a/sketch_mar26a.ino
+++ sketch_mar26a/sketch_mar26a.ino
@@ -2,58 +2,65 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-
+// Define fan pins
+#define fanPin 5
+#define DIRA 3
+#define DIRB 4
 void setup() {
   // put your setup code here, to run once:
+
+  //LCD STUFFS
   Wire.begin();
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("G");
+  lcd.print("Temp:");
   lcd.setCursor(0,1);
-  lcd.print("DST"); 
+  lcd.print("Humidity:");                            
+
+
+  // FAN INITTIALZIE
+  pinMode(fanPin, OUTPUT);
+  digitalWrite(fanPin, HIGH);
+   pinMode(DIRA,OUTPUT);
+
+  pinMode(DIRB,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-    String messages[] = {
-    "DST        ",
-    "Tara        ",
-    "Carry me    ",
-    "I'm a Wigfrid"
-  };
+  float temperature = 25.0; // temporary value
+  float humidity = 33.0; // temporary value
 
-  for(int i = 0; i<4; i++)
+// TESTING PURPOSES
+for(int i = 0; i <= 5; i++)
+{
+  temperature++;
+  delay(1000);
+
+  // LCD STUFFS
+  lcd.setCursor(0,0);
+  lcd.print("Temp: " );
+  lcd.print(temperature);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: " );
+}
+
+  // FAN LOGIC
+  if(temperature >= 5.0)
   {
-    lcd.setCursor(0,1);
-    lcd.print(messages[i]);
-    delay(1000);
+    digitalWrite(fanPin, HIGH);
+    digitalWrite(DIRA, HIGH);
+    digitalWrite(DIRB, LOW);
 
-    lcd.clear();
+  }
+  else
+  {
+    digitalWrite(fanPin, LOW);
+    digitalWrite(DIRA, HIGH);
+    digitalWrite(DIRB, LOW);
   }
 
-/* for(int i = 0; i<= 10; i++)
-{
-  lcd.setCursor(0,1);
-  lcd.print(i);
-  delay(1000);
-} */
-/*   lcd.setCursor(0,1);
-  lcd.print("Wilson");
-  delay(1000);
-  lcd.print("Willow");
-  delay(1000);
-  lcd.print("Wolfgang");
-  delay(1000);
-  lcd.print("Wendy");
-  delay(1000);
-  lcd.print("Woodie");
-  delay(1000);
-  lcd.print("Maxwell");
-  delay(1000);
-  lcd.print("Webber");
-  delay(1000);
-  lcd.print("Wigfried");
-  delay(1000); */
 }
