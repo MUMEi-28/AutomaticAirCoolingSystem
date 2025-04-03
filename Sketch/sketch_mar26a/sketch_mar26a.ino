65ce9105e37615998c3cd6045e6753d2dc716a75
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-04-03 22:30:08 +0800

    motor connection L298N

--- Sketch/sketch_mar26a/sketch_mar26a.ino
+++ Sketch/sketch_mar26a/sketch_mar26a.ino
@@ -1,16 +1,21 @@
-#include <Wire.h>
+#include <Wire.h> // I2C for LCD
 #include <LiquidCrystal_I2C.h> // LCD 1602 library
 #include "dht.h" // DHT11 temp & humid sensor library
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD object with 16cols x 2rows
 
 // Define fan pins
 #define fanPin 5
-#define DIRA 3
-#define DIRB 4
+#define DIRA 3 // Motor spin direction A
+#define DIRB 4// Motor spin direction B 
+
+// L298N DC motor pins
+#define MOTOR_ENA 9
+#define MOTOR_IN1 10
+#define MOTOR_IN2 11
 
 // Sensor
-dht DHT;
-#define DHT_PIN 7 // DHT11 data pin
+dht DHT; // DHT object
+#define DHT_PIN 7 // DHT11 data pin connected to pin 7
 
 // Global variables
 float temperature = 0;
@@ -19,43 +24,52 @@ float humidity = 0;
 void setup() {
   // put your setup code here, to run once:
 
-  // Fan ititialize
-  Serial.begin(9600);
   delay(500);//Delay to let system boot
+
+  // Initialize serial communication with to sensor
+  Serial.begin(9600);
   Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
 
-  //LCD STUFFS
-  Wire.begin();
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Temp:");
-  lcd.setCursor(0,1);
-  lcd.print("Humidity:");                            
-
+ //LCD STUFFS
+  Wire.begin(); // I2C communication
+  lcd.init(); // init LCD
+  lcd.backlight(); // turn on LCD
+  lcd.setCursor(0,0);  // move to col 0 and row 0
+  lcd.print("Temp:"); // print temp
+  lcd.setCursor(0,1); // move to col 0, row 1
+  lcd.print("Humidity:"); // print        
 
   // FAN INITTIALZIE
-  pinMode(fanPin, OUTPUT);
-  digitalWrite(fanPin, HIGH);
-   pinMode(DIRA,OUTPUT);
-
-  pinMode(DIRB,OUTPUT);
+  pinMode(fanPin, OUTPUT); // set to output 
+  digitalWrite(fanPin, HIGH); // default state is high 
+  pinMode(DIRA,OUTPUT); // set to output 
+  pinMode(DIRB,OUTPUT); // set to output 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-
+//  TemperatureHumiditySensor();
+TurnOnMotor();
 }
+
+void TurnOnMotor()
+{
+  analogWrite(MOTOR_ENA, 255); // analog to, pede magkaroon ng discrete values. if trippings then add speed control (:
+  digitalWrite(MOTOR_IN1, HIGH); // Pampaikot ng motor, forward direction
+  digitalWrite(MOTOR_IN2, LOW); // IDK, pero ayaw gumana kung wala to
+}
+
 void TemperatureHumiditySensor()
 {
-   temperature = DHT.temperature; 
-   humidity = DHT.humidity; 
+  delay(1000);//Wait before accessing Sensor
 
-  DHT.read11(DHT_PIN); // Get pin 7
+   temperature = DHT.temperature; // Read temp
+   humidity = DHT.humidity;  // Set humidity
+
+  DHT.read11(DHT_PIN); // Read data on pin 7
   
-  // PRINTING LANG KASI WALA LCD
+  // PRINTING LANG KASI WALA LCD [DITO NA PRINT SERIAL MONITOR YUNG SERIAL BEGIN SA TAAS]
   Serial.print("Current humidity = ");
   Serial.print(humidity);
   Serial.print("%  ");
@@ -63,7 +77,7 @@ void TemperatureHumiditySensor()
   Serial.print(temperature); 
   Serial.println("C  ");
   
-  delay(5000);//Wait 5 seconds before accessing sensor again.
+  delay(5000);//Wait 5 seconds MAXIMUM 2SEC, idk what happens if less than that and am not willing to find out.
 
 }
 
@@ -90,15 +104,13 @@ void fanLogic()
    // FAN LOGIC
   if(temperature >= 5.0)
   {
-    digitalWrite(fanPin, HIGH);
-    digitalWrite(DIRA, HIGH);
-    digitalWrite(DIRB, LOW);
+    digitalWrite(fanPin, HIGH); // turn fan on
+    digitalWrite(DIRA, HIGH); // set motor direction 
+    digitalWrite(DIRB, LOW); // idk
 
   }
   else
   {
-    digitalWrite(fanPin, LOW);
-    digitalWrite(DIRA, HIGH);
-    digitalWrite(DIRB, LOW);
+    digitalWrite(fanPin, LOW); // turn fan off
   } 
 }
